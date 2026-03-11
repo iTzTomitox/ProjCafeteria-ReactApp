c1bef5458d96174941b6e536dc2f1405f731ec64
Tomas Beron <wikipediatomitob@gmail.com>
2026-03-11 10:54:26 -0300

    Button Error 404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <div style={{padding:24}}>
       <h2>404 - Página no encontrada</h2>
       <p>La ruta solicitada no existe.</p>
+      <Link to='/'>Volver a Home</Link>
     </div>
   );
 };
